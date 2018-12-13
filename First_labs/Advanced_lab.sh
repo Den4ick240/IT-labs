d00b55757e03568127f7720b3423b39afbe8091c
Den4ick240 <zhigalov@ccfit.nsu.ru>
2018-12-13 20:37:01 +0700

    *commit message*

--- First_labs/Advanced_lab.sh
+++ First_labs/Advanced_lab.sh
@@ -1,2 +1,20 @@
 #!bin/bash
 wget http://factorized.net/crusoe.txt
+
+mkdir tmp
+cd tmp
+
+>ichi.txt
+>ni.txt
+>san.c
+>yon.sh
+>go.cpp
+>roku.py
+>nana.txt
+>he.js
+
+find -name "*.txt"
+
+cd ..
+rm -r tmp
+
---
+++ First_labs/Expert_lab.sh
@@ -0,0 +1,2 @@
+#!bin/bash
+ awk options program file
