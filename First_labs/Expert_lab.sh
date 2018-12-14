51866ebfbceaef90cdaf8942b04c4068aaddc549
Den4ick240 <zhigalov@ccfit.nsu.ru>
2018-12-14 10:08:24 +0700

    .

--- First_labs/Expert_lab.sh
+++ First_labs/Expert_lab.sh
@@ -1,2 +1,23 @@
+
 #!bin/bash
- awk options program file
+mkdir tmp
+cd tmp
+echo 'get file'
+wget http://factorized.net/crusoe.txt
+
+echo 'part one'
+echo 'count Fridays'
+grep -c "Friday" crusoe.txt
+
+echo 'part two'
+
+echo 'write Saturday instead of Friday'
+sed 's/Friday/Saturday/' ./crusoe.txt 1>crusoe.txt
+
+echo 'count Fridays one more time'
+grep -c "Friday" crusoe.txt
+
+echo 'part three'
+
+cd ..
+rm -r tmp
