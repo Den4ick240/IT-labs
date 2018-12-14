e9d321b8ab459dbdea3d71fe5dc0389fbc86424d
Den4ick240 <zhigalov@ccfit.nsu.ru>
2018-12-14 10:14:00 +0700

    have done expert lab

--- First_labs/Expert_lab.sh
+++ First_labs/Expert_lab.sh
@@ -18,6 +18,11 @@ echo 'count Fridays one more time'
 grep -c "Friday" crusoe.txt
 
 echo 'part three'
+echo '1 2
+2 2
+3 7' > input.txt
+awk '{s1+= $1; s2+= $2} END {print s1, s2}' ./input.txt
+
 
 cd ..
 rm -r tmp
